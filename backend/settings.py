8680bbef526220903af1958245b98d1ca88200a5
mehdi220320 <medmehdi1920@gmail.com>
2025-11-10 23:08:24 +0100

    ALLOW ALL CORS FOR DEVELOPMENT FASE ONLY

--- backend/settings.py
+++ backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 ALLOWED_HOSTS = ["*", "django.local", "127.0.0.1", "localhost"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
